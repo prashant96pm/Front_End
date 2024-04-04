2a9522d216328dde105c14d249e2a339f6aa6d06
Prashant Marabe <65422126+prashant96pm@users.noreply.github.com>
2024-04-04 07:35:36 +0530

    c

--- Meals-App-main/index.js
+++ Meals-App-main/index.js
@@ -121,7 +121,7 @@ function showFav() {
       mealName.innerText = meal.strMeal;
       mealElement.appendChild(mealName);
 
-      const mealIngredients = document.createElement('ul');
+      
       for (let i = 1; i <= 20; i++) {
           if (meal[`strIngredient${i}`]) {
               const ingredient = document.createElement('li');
